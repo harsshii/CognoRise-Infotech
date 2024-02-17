362e1cc5c950bba45b5a49d32039392a6330b712
Harshini Sridhar <119149817+harsshii@users.noreply.github.com>
2024-02-17 12:20:29 +0530

    Delete Task 2 directory

--- Task 2/src/Paddle.js
+++
@@ -1,8 +0,0 @@
-// Paddle.js
-import React from 'react';
-
-const Paddle = ({ x, y, height }) => {
-  return <div style={{ position: 'absolute', left: x, top: y, height: height, width: '10px', background: '#fff' }}></div>;
-};
-
-export default Paddle;
